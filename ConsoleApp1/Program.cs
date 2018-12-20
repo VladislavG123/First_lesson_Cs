e559e6a0854d1c0658a15b0474c4d62a78c62066
Горохов Владислав Олегович <@corp.itstep>
2018-12-20 17:50:45 +0600

    Создали репозитории

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1{
+    class Program {
+        static void Main(string[] args) {
+            string name = "Vlad";
+            int age = 15;
+            Console.WriteLine("Hello World");
+            Console.WriteLine("My Name - " + name + "\nMy age - " + age.ToString());
+            Console.Read();
+        }
+    }
+}//Git Repozitory
+                            //github               gitlab
